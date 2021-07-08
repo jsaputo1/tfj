682b0bdf395521a1ee4ca9def65bb91a62f7224e
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2021-07-07 21:54:52 -0400

    initial code and styling for header

--- themes/tfj/header.php
+++ themes/tfj/header.php
@@ -1,59 +1,77 @@
-<?php
-/**
- * The header for our theme
- *
- * This is the template that displays all of the <head> section and everything up until <div id="content">
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package tfj
- */
-
-?>
-<!doctype html>
-<html <?php language_attributes(); ?>>
+<!DOCTYPE html>
+<html <?php language_attributes();?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
-
-	<?php wp_head(); ?>
+    <?php wp_head();?>
+    <meta charset="<?php bloginfo('charset');?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 </head>
-
-<body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'tfj' ); ?></a>
-
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$tfj_description = get_bloginfo( 'description', 'display' );
-			if ( $tfj_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $tfj_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'tfj' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
+<body <?php body_class();?>>
+<header>
+	<div class="top-header">
+		<ul class="container mb-0">
+		<?php 
+		while (have_rows( 'social_links', 'option' )):
+			the_row();
+			foreach (get_sub_field( 'links', 'option' ) as $link): 
 			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+				<li>
+					<a class="nav-link" href="<?php echo $link['link']; ?>" target="_blank">
+						<img src="<?php echo get_template_directory_uri() . '/assets/images/'?><?php echo $link['icon']; ?>.svg" alt="<?php echo $link['icon']; ?>" class="icon">
+					</a>
+				</li>
+			<?php 
+			endforeach;
+			endwhile;
+			?>
+		</ul>
+	</div>
+	<div class="hero-header container">
+		<div class="row">
+			<div class="col-lg-5 left">
+				<h2><?php echo esc_html((get_field( 'header_title', 'option' ) )); ?></h2>
+				<h4><?php echo esc_html((get_field( 'header_subtitle', 'option' ) )); ?></h4>
+				<div class="footer">
+					<a class="btn btn-primary" href="#">Subscribe</a>
+					<div class="icon-list">
+					<?php 
+					while (have_rows( 'social_links', 'option' )):
+						the_row();
+						foreach (get_sub_field( 'links', 'option' ) as $link): 
+						?>
+							<a class="nav-link" href="<?php echo $link['link']; ?>" target="_blank">
+								<img src="<?php echo get_template_directory_uri() . '/assets/images/'?><?php echo $link['icon']; ?>-dark.svg" alt="<?php echo $link['icon']; ?>" class="icon">
+							</a>
+						<?php 
+						endforeach;
+						endwhile;
+						?>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-7 right">
+				<img src="<?php echo esc_url((get_field( 'hero_image', 'option' ))['url']); ?>" alt="<?php echo esc_attr((get_field( 'hero_image', 'option' ))['alt']); ?>" />
+			</div>
+		</div>
+	</div>
+	<nav class="navbar navbar-expand-lg navbar-light" role="navigation">
+		<div class="container">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#primary-nav-menu" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+				</button>
+					<?php
+					wp_nav_menu( array(
+							'theme_location'    => 'primary',
+							'depth'             => 1,
+							'container'         => 'div',
+							'container_class'   => 'collapse navbar-collapse',
+							'container_id'      => 'primary-nav-menu',
+							'menu_class'        => 'nav navbar-nav',
+							'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+							'walker'            => new WP_Bootstrap_Navwalker(),
+					) );
+					?>
+		</div>
+	</nav>
+</header>
