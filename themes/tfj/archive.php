eb9f2cc1fadc4452c6bf81ee9901b0fb9e6f441a
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2021-07-08 15:40:17 -0400

    added archive page

--- themes/tfj/archive.php
+++ themes/tfj/archive.php
@@ -10,42 +10,51 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
-
-		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
-
+<main id="primary" class="site-main container">
+	<div class="row">
+		<div class="col-lg-9 main-content border">
+			<h2><?php echo get_the_category()[0]->name; ?></h2>
 			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-	</main><!-- #main -->
-
+			$cat = the_category_id( false );
+			$args = array( 
+				'post_type'   => 'post', 
+				'order'       => 'ASC',
+				'cat'    	  => $cat
+				);
+			$latest_posts = get_posts( $args ); 
+			?>
+        	<?php 
+			foreach ( $latest_posts as $post ) : setup_postdata( $post ); 
+				?>
+				<div class="card row">
+					<div class="col-md-3">
+						<figure>
+							<a href="<?php echo the_permalink(); ?>"><?php echo get_the_post_thumbnail(); ?></a>
+						</figure>
+					</div>
+					<div class="col-md-9 body">
+						<div class="header">
+							<h6><?php print_r( get_the_category()[0]->name );?></h6>
+							<a href="<?php echo the_permalink(); ?>">
+								<h3><?php echo the_title(); ?></h3>
+								<?php echo the_excerpt(); ?>
+							</a>
+						</div>
+						<div class="footer">
+							<div class="post-details">
+								<h5><?php echo get_the_author_meta('display_name', 1); ?></h5>
+								<h6><?php echo the_date(); ?></h5>
+							</div>
+						</div>
+					</div>
+				</div>
+			<?php endforeach; ?>
+	
+		</div>
+		<div class="col-lg-3 sidebar border">
+			<?php get_sidebar();?>
+		</div>
+	</div>
+</main><!-- #main -->
 <?php
-get_sidebar();
 get_footer();
