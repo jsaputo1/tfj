a5267e97a123a1c43435cd78e88145c2f21bbf9b
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2021-07-09 13:48:30 -0400

    styled mobile view for single post

--- themes/tfj/single.php
+++ themes/tfj/single.php
@@ -15,9 +15,19 @@ get_header();
 		<div class="col-lg-9 main-content border">
 			<div class="header">
 				<h2><?php the_title(); ?></h2>
-				<div class="post-details">
-					<h5><?php echo get_the_author_meta('display_name', 1); ?></h5>
-					<h6><?php echo the_date(); ?></h5>
+				<div class="header-bottom">
+					<div class="post-details">
+					<a href="<?php echo get_author_posts_url( get_the_author_meta( 'id', 1 ) ); ?>"><h5><?php echo get_the_author_meta( 'display_name', 1 ); ?></h5></a>
+						<figure>
+							<a href="<?php echo get_the_author_meta( 'url', 1 ); ?>" target="blank">
+								<img src="<?php echo get_template_directory_uri() . '/assets/images/twitter-blue.svg'?>" />
+							</a>
+						</figure>
+						<h6><?php echo the_date(); ?></h5>
+					</div>
+					<div class="share-links">
+						<div class="sharethis-inline-share-buttons"></div>
+					</div>
 				</div>
 			</div>
 			<div class="body">
