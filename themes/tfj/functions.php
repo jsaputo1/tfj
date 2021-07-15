0deac1f3905c1930cac51ea65755d4c8f596f468
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2021-07-14 21:47:38 -0400

    added local acf json

--- themes/tfj/functions.php
+++ themes/tfj/functions.php
@@ -212,4 +212,16 @@ add_filter( 'get_the_archive_title', function ($title) {
 	return $title;    
 });
 
+//save acf json file
+add_filter('acf/settings/save_json', 'my_acf_json_save_point');
+ 
+function my_acf_json_save_point( $path ) {
+
+    $path = get_stylesheet_directory() . '/acf-json';
+    // return
+    return $path;
+    
+}
+
+
 ?>
